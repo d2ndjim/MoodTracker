2dd75c88ee955d5d5fd7c3e1f609a2645753539e
Lekan <jimoridwanlekan@gmail.com>
2023-04-22 16:20:29 +0100

    Add assests folder

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
+
+const image = require('../../assets/images/butterflies.png');
 
 export const Home: React.FC = () => {
   const appContext = useAppContext();
 
   return (
     <View style={styles.container}>
+      <Image source={image} />
       <MoodPicker handleSelectMood={appContext.handleSelectMood} />
     </View>
   );
