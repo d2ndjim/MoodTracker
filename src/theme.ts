1d21b49a88cb7f1a7cfbbc2831957431afd1001d
Lekan <jimoridwanlekan@gmail.com>
2023-04-23 21:15:26 +0100

    Update theme file

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,12 @@
 export const theme = {
+  //colors
   colorPurple: '#454C73',
   colorWhite: '#fff',
   colorLavender: '#87677B',
   colorBlue: '#1D84B5',
   colorGrey: '#8E9AAF',
+
+  //themes
   fontFamilyBold: 'Kalam-Bold',
   fontFamilyRegular: 'Kalam-Regular',
   fontFamilyLight: 'Kalam-Light',
