96e4f59e55f6f1adebfcab8b9e9767ab628639f9
Lekan <jimoridwanlekan@gmail.com>
2023-04-26 15:57:16 +0100

    Add history animation with layout animation

--- src/screens/History.screen.tsx
+++ src/screens/History.screen.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { useAppContext } from '../App.provider';
 import { MoodItemRow } from '../components/MoodItemRow';
 
 export const History: React.FC = () => {
   const appContext = useAppContext();
   return (
-    <View>
-      {appContext.moodList.map(item => (
-        <MoodItemRow item={item} key={item.timestamp} />
-      ))}
-    </View>
+    <ScrollView style={styles.history}>
+      {appContext.moodList
+        .slice()
+        .reverse()
+        .map(item => (
+          <MoodItemRow
+            handleDeleteMood={appContext.handleDeleteMood}
+            item={item}
+            key={item.timestamp}
+          />
+        ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  history: {
+    marginTop: 10,
+  },
+});
